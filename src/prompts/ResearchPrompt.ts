6077ef749d31913276a4e804515b7fad950590ca
mikechao <mike.chao.one@gmail.com>
2025-04-29 11:44:14 -0700

    Basic steps for research prompt

--- src/prompts/ResearchPrompt.ts
+++ src/prompts/ResearchPrompt.ts
@@ -3,10 +3,13 @@ export function getResearchPrompt(topic: string) {
     + `\nYou have the following tools available to you:`
     + `\n'brave_web_search' - A web search tool that can be used to find information on the internet. Ideal for general queries`
     + `\n'brave_news_search' - Searches for news and trending topics. Ideal for recent events or specific news stories`
-    + `\nbrave_image_search - Searches for images on the internet. Ideal for finding images related to a topic`
+    + `\n'brave_image_search' - Searches for images on the internet. Ideal for finding images related to a topic`
     + `\n'brave_video_search' - Searches for videos on the internet. Ideal for finding videos related to a topic`
     + `\n1. Come up with a research plan for the topic of ${topic}. Present the research plan to the user.`
     + `\n  a. Iterate with the user until they are satisfied with the research plan.`
-
+    + `\n  b. Once the user is satisfied with the research plan proceed to step 2`
+    + `\n2. Generate 2 or 3 queries for each part of the research plan for the tools you have available to you.`
+    + `\n3. Use the tools and the generated queries to find information on the topic.`
+    + `\n4. Present the information you found to the user in a clear and concise manner.`
   ;
 }
