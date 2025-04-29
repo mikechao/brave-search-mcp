cf8ddf480fa839c0bfdd8165d2a33b02fae11f78
mikechao <mike.chao.one@gmail.com>
2025-04-29 15:36:20 -0700

    Added NoteTakingTool to the reasearch prompt

--- src/prompts/ResearchPrompt.ts
+++ src/prompts/ResearchPrompt.ts
@@ -5,11 +5,14 @@ export function getResearchPrompt(topic: string) {
     + `\n'brave_news_search' - Searches for news and trending topics. Ideal for recent events or specific news stories`
     + `\n'brave_image_search' - Searches for images on the internet. Ideal for finding images related to a topic`
     + `\n'brave_video_search' - Searches for videos on the internet. Ideal for finding videos related to a topic`
+    + `\n'note_taking' - A tool for taking notes on a web page and related topic. Ideal for summarizing information about a topic from a specific page`
     + `\n1. Come up with a research plan for the topic of ${topic}. Present the research plan to the user.`
     + `\n  a. Iterate with the user until they are satisfied with the research plan.`
     + `\n  b. Once the user is satisfied with the research plan proceed to step 2`
     + `\n2. Generate 2 or 3 queries for each part of the research plan for the tools you have available to you.`
-    + `\n3. Use the tools and the generated queries to find information on the topic.`
-    + `\n4. Present the information you found to the user in a clear and concise manner by using a HTML artifact format if possible.`
+    + `\n3. Use the tools and the generated queries to find URLS on the topic.`
+    + `\n4. Use the 'note_taking' tool to take notes on the URLS and topics you found.`
+    + `\n5. From the notes you took, generate a summary of the information you found.`
+    + `\n6. Present the information you found to the user in a clear and concise manner by using a HTML artifact format if possible.`
   ;
 }
